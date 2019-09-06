dc1f0985d4e2c1f2cbe7efed3d227d704273eada
Argonus <argonuspiotr@gmail.com>
2019-09-06 11:37:25 +0200

    Envoke function body in quote for test env

--- lib/new_relic_addons/decorators.ex
+++ lib/new_relic_addons/decorators.ex
@@ -94,10 +94,8 @@ defmodule NewRelicAddons.Decorators do
   # For test env we are not creating another tasks, as Ecto in shared mode is unable to close them.
   defp start_new_relic_transaction(:test, category_name, name, body) do
     quote do
-      fn ->
-        NewRelic.start_transaction(unquote(category_name), unquote(name))
-        unquote(body)
-      end
+      NewRelic.start_transaction(unquote(category_name), unquote(name))
+      unquote(body)
     end
   end
 
